4b3f6a9c3725cfccb6d28d2174c69bb2fb622d37
Dale-Kurt Murray <dmurray@frame.io>
2020-04-08 15:35:43 -0400

    Updated

--- vscode.symlink/extensions/amazonwebservices.aws-toolkit-vscode-1.0.0/out/src/shared/environmentVariables.js
+++
@@ -1,7 +0,0 @@
-/*!
- * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=environmentVariables.js.map
